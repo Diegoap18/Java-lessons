4b3be3152b7f99f71ef6b3684b863f82a27f4772
Hunnilysoft <56772594+Diegoap18@users.noreply.github.com>
2025-05-14 14:45:25 -0300

    lesson 1 - como declarar uma variavel
    exemplo de como declarar e execução realizada.

--- Curso de Java/src/Main.java
+++ Curso de Java/src/Main.java
@@ -1,7 +1,19 @@
+import javax.naming.ldap.UnsolicitedNotification;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        //TIPO DE DADO             + IDENTIFICAÇÃO + VALOR A SER ATRIBUIDO
+        // VOU ARMAZENAR UMA STRING +  nome         + "Diego A Pereira"
+        // o nome da variavel sempre é minuscula e apartir da segunda é maistula exemplo:
+        // nome = minuscula, nomeSobrenome = minuscula e maiscula. e tb pode colocar underline _
+        // nome_sobrenome = esse tambem é aceitavel.
+        
+        String nome = "Diego A Pereira";
+
+        //printando o nome na tela seu utiliza o println atalho soutm
+        System.out.println(nome);
     }
 }
