3351b4af52bf64bc11d37722d3ac174e4f0455fb
Hunnilysoft <56772594+Diegoap18@users.noreply.github.com>
2025-05-17 20:37:13 -0300

    operadores relacionais
    operadores relacionais, igual, diferente, maior, menor, menor ou igual, maior ou igual.

--- Curso de Java/src/Main.java
+++ Curso de Java/src/Main.java
@@ -3,5 +3,23 @@
 public class Main {
     public static void main(String[] args) {
 
+        /*
+            Operadores Relacionais:
+            Operadores de igualdade (==, !=),
+            so pode retornar valores booleanos, verdadeiro ou falso
+
+            Operadores Relacionais  (>,>=,<,<=)
+         */
+
+        int numero1 = 5;
+        int numero2 = 10;
+        System.out.println(numero1 == numero2); //true or false
+        System.out.println(numero1 != numero2); //true or false
+        System.out.println(numero1 > numero2); //true or false
+        System.out.println(numero1 >= numero2); //true or false
+        System.out.println(numero1 < numero2); //true or false
+        System.out.println(numero1 <= numero2); //true or false
+        
+
     }
 }
