cf4b583a4733af71b4c4d6edb72e9f88cc492379
Hunnilysoft <56772594+Diegoap18@users.noreply.github.com>
2025-05-17 19:39:56 -0300

    como comentar e tipos de comentação
    comentando formas diferentes:

--- Curso de Java/src/Main.java
+++ Curso de Java/src/Main.java
@@ -3,5 +3,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        /*
+        Para comentar seu codigo voce pode usar o //
+        ou (/ *  * /) (esta separado para nao dar problema)
+        dessa forma ele vai comentar.
+        exemplo abaixo:
+        */
+
+        long numeroDeConta = 1455487484L;
+        // numeroDeConta = 14344112123L;
+
+        /* numeroDeConta: utilizado para armazenar numero de conta
+        do cliente.
+        */
+
+        System.out.println(numeroDeConta);
+
+
     }
 }
