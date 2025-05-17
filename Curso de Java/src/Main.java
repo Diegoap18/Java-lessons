ce052d0b8dc16aa290bb4db017f24cb0363de0e6
Hunnilysoft <56772594+Diegoap18@users.noreply.github.com>
2025-05-17 20:10:39 -0300

    concatenação e Scanner
    aprendemos a concatenar os dados e  a utilizar o scanner para capturar e mostrar os dados do usuario no terminal.

--- Curso de Java/src/Main.java
+++ Curso de Java/src/Main.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        //Parte 1: contatenação de variaveis dentro do system.out.println(soutm).
+        String nome = "Diego Aparecido Pereira";
+        Byte idade = 36;
+        String profissao = "Desenvolvedor Java";
+        System.out.println("App Parte1: " + "Meu Nome é " + nome + ", eu tenho " + idade + " anos" + " e sou " + profissao);
+
+        //Parte 2 usando o Scanner para capturar a entrada do usuario:
+
+        String nomeUsuario;
+        String idadeUsuario; // usando String no lugar de Bytes por conta do nextlines()
+        String profissaoUsuario;
+
+        Scanner entradaUsuario = new Scanner(System.in);
+
+        System.out.println("Qual o seu Nome?");
+        nomeUsuario = entradaUsuario.nextLine();
+
+        System.out.println("Qual sua idade?");
+        idadeUsuario = entradaUsuario.nextLine();
+
+        System.out.println("Qual Sua profissão");
+        profissaoUsuario = entradaUsuario.nextLine();
+
+        System.out.println("App Parte2: " + "Meu Nome é " + nomeUsuario + ", eu tenho " + idadeUsuario + " anos" + " e sou " + profissaoUsuario);
     }
+    //
 }
