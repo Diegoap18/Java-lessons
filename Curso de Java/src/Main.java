b1ddd5ddfb3c2d9f78b298d74ae0635bb3aaca82
Hunnilysoft <56772594+Diegoap18@users.noreply.github.com>
2025-05-17 19:30:34 -0300

    exemplos de dados mutaveis e imutaveis
    exemplos

--- Curso de Java/src/Main.java
+++ Curso de Java/src/Main.java
@@ -3,5 +3,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        //variavel mutavel, voce muda o valor varias vezes ou
+        //quando quiser durante a execução.
+        byte idade = 30;
+        idade = 50;
+        idade = 120;
+        System.out.println(idade);
+
+        //Variavel imutaveis,são variaveis que não pode ser alteradas
+        //durante a execução: para isso inclua o final no inicio como abaixo:
+        // sempre incluir com letra maiuscula
+        // sempre utilize para dados fixos que não seram alterados na aplicação
+        final String NOME = "Carlos";
+        System.out.println(NOME);
     }
 }
