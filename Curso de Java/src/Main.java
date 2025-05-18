65845d1df33add16b74829fbf20e3bd7b6f7536f
Hunnilysoft <56772594+Diegoap18@users.noreply.github.com>
2025-05-17 21:28:20 -0300

    estrutura while
    while looping

--- Curso de Java/src/Main.java
+++ Curso de Java/src/Main.java
@@ -1,7 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        /*
+            Estrutura de repetição
+            1- While: enquanto:
+            example:
+            while(condição){
+             codigo para execução.
+            }
+
+         */
+
+        //while com array:
+        int contador = 0;
+
+        while (contador <= 10) {//true
+            contador++;
+            System.out.println(contador);
+        }
+
+        //while com array:
+        int indice = 0;
+        String[] listaDeCompras = {"banana","carne","ovos","arroz"};
+
+        while (listaDeCompras.length <= 4) {//true
+            System.out.println(listaDeCompras[indice]);
+            indice++;
+        }
 
     }
 }
