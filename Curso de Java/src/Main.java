01421c95a7a4795cedb4e7ea3bb4fbb3dfab8e73
Hunnilysoft <56772594+Diegoap18@users.noreply.github.com>
2025-05-17 21:06:48 -0300

    operadores ou e and
    operadores importantes.

--- Curso de Java/src/Main.java
+++ Curso de Java/src/Main.java
@@ -3,5 +3,40 @@
 public class Main {
     public static void main(String[] args) {
 
+        /*
+            Operadores Logicos (|| e &&) 
+                                ou   and
+         */
+
+        int numero1 = 10;
+        int numero2 = 20;
+        int numero3 = 30;
+        int numero4 = 40;
+
+        if (numero1 == numero2 || numero3 < numero4) {
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("Falso");
+        }
+
+        if (numero1 == numero2 || numero3 == numero4) {
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("Falso");
+        }
+
+        if (numero1 == numero2 && numero3 == numero4) {
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("Falso");
+        }
+
+        if (numero1 < numero2 && numero3 == numero4) {
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("Falso");
+        }
+
+
     }
 }
