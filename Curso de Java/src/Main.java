c6de9780ff0b539a6220a7fb6f1363ccd5e4d2b3
Hunnilysoft <56772594+Diegoap18@users.noreply.github.com>
2025-05-17 21:33:26 -0300

    estrurua for
    for utilizado para loop.

--- Curso de Java/src/Main.java
+++ Curso de Java/src/Main.java
@@ -2,6 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        /*
+            For:
+         */
+        String[] listaDeCompras = {"banana","carne","ovos","arroz"};
 
+        for (int indice = 0; listaDeCompras.length <= 4; indice++) {
+            System.out.println(listaDeCompras[indice]);
+        }
     }
 }
